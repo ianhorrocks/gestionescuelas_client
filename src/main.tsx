23acce27e3e5d4a80fb670ea6f05444e9afb4b6e
ianhorrocks <1504967@ucc.edu.ar>
2025-04-18 17:19:55 -0300

    api deploy test

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import "./styles/styles.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
+// DEBUG: Verificar si Doppler está inyectando bien las variables
+console.log("VITE_API_ENV:", import.meta.env.VITE_API_ENV);
+console.log("VITE_API_URL_LOCAL:", import.meta.env.VITE_API_URL_LOCAL);
+console.log("VITE_API_URL_PROD:", import.meta.env.VITE_API_URL_PROD);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
